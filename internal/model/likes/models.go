37ad5c722b92aaff55980b8ab1e1a94947ec0fc6
AlfariziDwiPrasetyo <yoerdanatan@gmail.com>
2025-10-26 10:48:02 +0700

    feat : add likes model

---
+++ internal/model/likes/models.go
@@ -0,0 +1,10 @@
+package likes
+
+import "time"
+
+type Like struct {
+	ID        int64     `gorm:"primaryKey"`
+	PostID    int64     `gorm:"not null; index; uniqueIndex:idx_user_post"`
+	UserID    int64     `gorm:"not null; index; uniqueIndex:idx_user_post"`
+	CreatedAt time.Time `gorm:"created_at"`
+}
--- pkg/database/migrate.go
+++ pkg/database/migrate.go
@@ -5,13 +5,19 @@ import (
 
 	"github.com/alfarizidwiprasetyo/be-umc-learn/internal/model/authentications"
 	"github.com/alfarizidwiprasetyo/be-umc-learn/internal/model/comments"
+	"github.com/alfarizidwiprasetyo/be-umc-learn/internal/model/likes"
 	"github.com/alfarizidwiprasetyo/be-umc-learn/internal/model/posts"
 	"github.com/alfarizidwiprasetyo/be-umc-learn/internal/model/users"
 	"gorm.io/gorm"
 )
 
 func Migrate(db *gorm.DB) {
-	if err := db.AutoMigrate(&users.User{}, &authentications.Authentications{}, &posts.Post{}, &comments.Comments{}); err != nil {
+	if err := db.AutoMigrate(
+		&users.User{},
+		&authentications.Authentications{},
+		&posts.Post{},
+		&comments.Comments{},
+		&likes.Like{}); err != nil {
 		log.Fatal("Failed to migrate: ", err)
 	}
 }
